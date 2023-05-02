57a8058ee9e6895a2f4d66ee35c9354c4d5645b9
Lui10dya <120044367+Lui10dya@users.noreply.github.com>
2023-05-02 14:25:47 +0200

    Player, Inventory and InventoryObj

---
+++ player/Inventory.js
@@ -0,0 +1,43 @@
+//@author: Luis
+import * as THREE from 'three';
+import {InventoryObj} from './InventoryObj.js'
+
+export class Inventory {
+    #mass = 0;
+    #items;
+
+    constructor() {
+        this.#items = new Array();
+    }
+
+    addItem(newItem) {
+        index = this.#items.findIndex((item) => item.constructor.name == newItem.constructor.name)
+        if (index == -1) {
+            this.#items.push(newItem);
+        } else {
+            this.#items[index].amount += newItem.amount;
+        }
+        this.#mass += newItem.totalMass();
+    }
+
+    removeItem(oldItem) {
+        index = this.#items.findIndex((item) => item.constructor.name == oldItem.constructor.name)
+        if (index == -1) {
+            return error;
+        } else {
+            this.#items[index].amount -= newItem.amount;
+            this.#mass -= oldItem.totalMass();
+        }
+    }
+
+    getRenderableInventoryElement() {
+        var inventory = new Element(
+            //TODO
+        );
+        return Element;
+    }
+
+    getMass() {
+        return this.#mass;
+    }
+}
