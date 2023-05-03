d22a37dcc81ba8dc5397b39d338a9cfa0c71bec6
Lui10dya <800466@unizar.es>
2023-05-03 11:00:50 +0200

    Messages updated

--- communication/message/MovementMessage.js
+++ communication/message/MovementMessage.js
@@ -7,26 +7,26 @@
 class MovementMessage extends Message {
     constructor(keyPressed) {
         switch (keyPressed) {
-            case 32:
+            case 32:    // Space
                 this.#content = "jump";
                 break;
-            case 87:
-            case 119:
+            case 87:    // w
+            case 119:   // W
                 this.#content = "forward";
                 break;
-            case 65:
-            case 97:
+            case 65:    // a
+            case 97:    // A
                 this.#content = "left";
                 break;
-            case 83:
-            case 115:
+            case 83:    // s
+            case 115:   // S
                 this.#content = "backward";
                 break;
-            case 68:
-            case 100:
+            case 68:    // d
+            case 100:   // D
                 this.#content = "right";
                 break;
-            case 15:
+            case 15:    // Shift
                 this.#content = "sprint";
                 break;
         }
--- communication/message/OverlayMessage.js
+++ communication/message/OverlayMessage.js
@@ -7,11 +7,11 @@
 class OverlayMessage extends Message {
     constructor(keyPressed) {
         switch (keyPressed) {
-            case 27:
+            case 27:    // ESC
                 this.#content = "escape";
                 break;
-            case 77:
-            case 109:
+            case 77:    // m
+            case 109:   // M
                 this.#content = "map";
                 break;
         }
