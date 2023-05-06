725993341f50d5fad50c5355e75a87d7fbf323bb
Malte Kerl <kerl@rhrk.uni-kl.de>
2023-05-06 08:23:06 +0200

    Create Setting Class

---
+++ overlays/Setting.js
@@ -0,0 +1,31 @@
+import MenuMediator from "../communication/mediator/MenuMediator";
+
+export default class Setting {
+    #value;
+    #displayName;
+    #mediator;
+
+
+    constructor(displayName) {  
+        this.#displayName = displayName;
+        this.#mediator = MenuMediator.getInstance();
+    }
+
+    setValue(value) {
+        this.#value = value;
+        this.sendMessage;
+    }
+
+    getDisplayName() {
+        return this.#displayName;
+    }
+
+    sendMessage() {
+        messageObj = {
+            setting: this.#displayName,
+            value: this.#value
+        }
+        message = new PlayerMessage(JSON.stringify(messageObj));
+        this.#mediator.notify(message);
+    }
+}
