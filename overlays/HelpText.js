526dac92c631c0dcd58eb84823a2145418c6f845
TedGran <tedsele2000@hotmail.com>
2023-05-06 08:30:07 +0200

    HelpText should be done.

--- overlays/HelpText.js
+++ overlays/HelpText.js
@@ -1,12 +1,45 @@
+/** @author: Ted */
 /**
  * @class HelpText
- * @extends {Overlay}
- * 
+ * @extends Overlay
  */
 
 require('./Overlay');
 
 import Overlay from './Overlay';
 
-export default class HelpText extends Overlay{
+export default class HelpText extends Overlay {
+
+    /**
+    * 
+    * @param {RGB color} color 
+    */
+    setFontColor(color) {
+        this.fontColor = color;
+    }
+    
+    /**
+    * 
+    * @param {String} text
+    */
+    setText(text) {
+        this.text = text;
+    }
+
+    /**
+    * 
+    * @param {Number} size
+    */
+    setFontSize(size) {
+        this.fontSize = size;
+    }
+
+    /**
+    * 
+    * @param {String} font
+    */
+    setFont(font) {
+        this.font = font;
+    }
+
 }
