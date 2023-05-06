ec5bb3dc574f64e2618f99e09818a6801e1b3e77
GabHaf <74302691+GabHaf@users.noreply.github.com>
2023-05-06 08:50:24 +0200

    SettingAffectingMenue

--- overlays/SettingAffectingMenu.js
+++ overlays/SettingAffectingMenu.js
@@ -1,12 +1,41 @@
-/**
- * @class SettingAffectingMenu
- * @extends {Overlay}
- * 
- */
-
 require('./Overlay');
+require('./FullscreenMenu');
+require('./Setting');
 
 import Overlay from './Overlay';
+import FullscreenMenu from './FullscreenMenu';
+import Setting from './Setting';
 
-export default class SettingAffectingMenu extends Overlay{
+/**
+ * @class SettingAffectingMenu
+ * @extends {FullscreenMenu}
+ * 
+ */
+export default class SettingAffectingMenu extends FullscreenMenu{
+
+    constructor(){
+        this.affectableSettings = new Array();
+    }
+    
+
+    /**
+     * 
+     * @param {Setting} setting 
+     */
+    addAffectableSetting(setting){
+        this.affectableSettings.push(setting);
+
+        renderable = new Element(setting.getDisplayName());
+        renderable.createTextNode(setting.getDisplayName());
+        slider = new Element();
+        slider.innerHTML = '< input type="range" min="0" max="100" class="Slider" >';
+        slider.addEventListener("onmouseup", function() {
+            this.setting.setValue(slider.value);
+            this.setting.sendMessage();
+        });
+        
+        this.addElement();
+    }
+
+
 }
